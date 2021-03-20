696b37dcf00f677e07197dabb786f7ff26377d28
nash23234 <80130125+nash23234@users.noreply.github.com>
2021-03-20 15:16:44 -0600

    Update portafolio 1.py

--- portafolio 1.py
+++ portafolio 1.py
@@ -1,3 +1,9 @@
+"""
+Nombre: Divisores 
+Entrada: Num
+Salida: resultado de la división
+Restricción<: Número entero positivo
+"""
 def divisores(num):
     if(isinstance(num,int) and num>0):
         return divisores_aux(num,num)
@@ -12,6 +18,12 @@ def divisores_aux(num,contador):
             print(contador)
         return divisores_aux(num,contador-1)
 #------------------------------------------------------------------------------------------------------    
+"""
+Nombre: Multiplicar Recursivo 
+Entrada: Num,factor
+Salida: El factor de los número 
+Restricción: Número entero positivo
+"""
 def multiplicarRecursivo(num,factor):
     if(isinstance(num,int) and isinstance(factor,int)):
         return multiplicar_aux(num,factor)
@@ -24,21 +36,13 @@ def multiplicar_aux(num,factor):
     else:
         return num+multiplicar_aux(num,factor-1)
 #--------------------------------------------------------------------------------------------------------
-"""
-#divisor    = 6
-#dividendo  = 2
-#contador   = 0
-#fin -> contador == divisor
 
-1 + (10 | 2 | 0+2)                      -> contador = 2 | 1
-1 + (1 + (10 | 2 | 2+2))                -> contador = 4 | 1 + (1)
-1 + (1 + (1 + (10 | 2 | 4+2)))          -> contador = 6 | 1 + (1 + (1))
-1 + (1 + (1 + (1 + (10 | 2 | 6+2))))    -> contador = 8 | 0 + (1 + (1 + (1))) = 3
-1 + (1 + (1 + (1 + (1 + (10 | 2 | 6+2)))))
-1 + (1 + (1 + (1 + (1 + (1 + (10 | 2 | 8+2))))))
-1 + (1 + (1 + (1 + (1 + (1 + (1 + (10 | 2 | 8+2)))))))
 """
-
+Nombre: división Recursivo
+Entrada: divisor,dividendo 
+Salida: La división entera de los números
+Restricción<: Número entero positivo
+"""
 def divisionRecursivo(divisor,dividendo):
     if(isinstance(divisor,int) and isinstance(dividendo,int)):
         return divisionRecursivo_aux(divisor,dividendo,0)
